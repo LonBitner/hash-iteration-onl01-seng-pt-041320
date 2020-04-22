8f696bf97317c1365f6d50755f6ce2ed99b2f5b1
Lon Bitner <lonbitner@gmail.com>
2020-04-22 20:12:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age > 12
-  end
     puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
+end
 end
